83279c7c59e57c41577258b56ad42a10ea9007f0
DeybiSuanca07 <deibymfc18@gmail.com>
2023-01-25 18:40:28 -0500

    Agregar archivos de proyecto.

---
+++ Miscelanea/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Miscelanea
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Menu de la aplicación
+            //El usuario ingresara una opción para ejecutar el ejercicio que necesita
+
+            //NumeroParEImpar numeroParEImpar = new NumeroParEImpar();
+            //numeroParEImpar.Execute();
+        }
+    }
+}
